52688e1abad369b98b91e55326559db9ca18e9f1
Tasha Lawson <tlawson@chicagotechacademy.org>
2014-05-01 13:01:43 -0500

    40weekexam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Week40exam::Application.config.secret_key_base = 'd1422f5d7692874e0a968ce121a9cc10a609c41ebb49b47d873c95182f7680d0897259be9870b8bbc0af064dba21a39871bf18f65808f0b6fee406a4da22adee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Week40exam::Application.config.session_store :cookie_store, key: '_week40exam_session'
